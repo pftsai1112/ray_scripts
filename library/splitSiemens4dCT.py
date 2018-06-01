494ecd6a670ee991f62a880c09dd8115c50be616
Adam Bayliss <rabayliss@wisc.edu>
2018-06-01 15:26:19 -0500

    Added Header

--- library/splitSiemens4dCT.py
+++ library/splitSiemens4dCT.py
@@ -1,3 +1,40 @@
+""" Split Siemens 4D CT
+    
+    Divide the Siemens 4D into up to 13 phases
+    
+    Scope: Specific directories required.  Testing compatibility with dicom library
+
+    Example Usage:
+
+    This program is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later
+    version.
+    
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License along with
+    this program. If not, see <http://www.gnu.org/licenses/>.
+    """
+
+__author__ = 'Adam Bayliss'
+__contact__ = 'rabayliss@wisc.edu'
+__date__ = '2018-Apr-06'
+__version__ = '1.0.0'
+__status__ = 'Development'
+__deprecated__ = False
+__reviewer__ = 'Someone else'
+__reviewed__ = 'YYYY-MM-DD'
+__raystation__ = '7.0.0'
+__maintainer__ = 'One maintainer'
+__email__ =  'rabayliss@wisc.edu'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright (C) 2018, University of Wisconsin Board of Regents'
+__credits__ = ['']
+
+
 from connect import *
 import tempfile
 import zipfile
